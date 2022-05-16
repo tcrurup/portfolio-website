7b1e6dfc05efe25a2ad363bbeaea82998d9227ad
tcrurup <tcrurup@gmail.com>
2022-05-16 13:43:54 -0600

    will used template html on bundle

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-      new HtmlWebpackPlugin()
+      new HtmlWebpackPlugin({template: "./src/index.html"})
   ]
 };
