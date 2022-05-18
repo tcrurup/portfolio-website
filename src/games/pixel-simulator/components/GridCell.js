d9825819b9bca5fe63c9fcca0bb4ae327d27e2ca
tcrurup <tcrurup@gmail.com>
2022-05-18 12:16:04 -0600

    add debug mode to draw grid on canvas

---
+++ src/games/pixel-simulator/components/GridCell.js
@@ -0,0 +1,8 @@
+export default class GridCell{
+
+    constructor(color = "black"){
+        this.color = color
+    }
+
+}
+
--- src/games/pixel-simulator/containers/PixelSimulatorDisplay.js
+++ src/games/pixel-simulator/containers/PixelSimulatorDisplay.js
@@ -1,32 +1,50 @@
+import GridCell from "../components/GridCell.js";
+
+const CELL_HEIGHT_PIXELS = 16;
+const CELL_WIDTH_PIXELS = 16;
+const CELL_HEIGHT_COUNT = 50;
+const CELL_WIDTH_COUNT = 50;
+const DEBUG = true;
+
 export default class PixelSimulatorDisplay{
 
     #displayElement
+    #grid
 
     constructor(){
-        console.log("initializing display")
         let elem = document.createElement("canvas")
-        elem.width=800
-        elem.height=800
+        elem.width= this.width
+        elem.height=this.height
         elem.className = "pixel-simulator-display"
-        elem.addEventListener("mousemove", this.mouseMove)
         this.#displayElement = elem
-        console.log(this.context)
+        this.initialize()
     }
 
     get element(){ return this.#displayElement }
     get context(){ return this.element.getContext('2d')}
+    get height(){ return CELL_HEIGHT_PIXELS * CELL_HEIGHT_COUNT }
+    get width(){ return CELL_WIDTH_PIXELS * CELL_WIDTH_COUNT }
 
-    mouseMove = event =>{
-        const coords = {
-            x: event.offsetX,
-            y: event.offsetY
-        }
-        this.colorPixel(coords)
+    initialize(){
+        this.#grid = new Array(CELL_WIDTH_COUNT).fill(new Array(CELL_HEIGHT_COUNT).fill(new GridCell()))
+        if(DEBUG){ this.drawGrid() }
     }
     
-    colorPixel(coords){
-        console.log(coords)
-        this.context.fillRect(coords.x, coords.y, 1, 1)
+    updateDisplay(){
+
+    }
+
+    drawGrid(){
+        //Draw horizontal lines
+        for(let i=0; i< this.height; i+=CELL_HEIGHT_PIXELS){
+            this.context.fillRect(0, i, this.width, 1)
+        }
+
+        //Draw vertical lines
+        for(let i=0; i< this.height; i+=CELL_HEIGHT_PIXELS){
+            this.context.fillRect(i, 0, 1, this.height)
+        }
+
     }
 
     static create(){
